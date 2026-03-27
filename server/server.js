fcf861f6622724fa535ab0d1f45a9dde50fa1045
Hemaghara <hemaghara@gmail.com>
2026-03-27 15:25:12 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -16,20 +16,7 @@ const PORT = process.env.PORT || 5000;
 const io = initSocket(server);
 app.set("io", io);
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-    ],
-    exposedHeaders: ["Content-Range", "X-Content-Range"],
-  }),
-);
+app.use(cors());
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
