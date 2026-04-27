31c061c88cf617de1c86bc92d11a546d58f2b6a3
Hemaghara <hemaghara@gmail.com>
2026-04-27 18:33:19 +0530

    commit

--- client/src/pages/BusinessDetails.jsx
+++ client/src/pages/BusinessDetails.jsx
@@ -41,15 +41,15 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import Highlights from "../components/Highlights";
 import { BusinessDetailsSkeleton } from "../components/Skeleton";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
 });
 
 const BusinessDetails = () => {
